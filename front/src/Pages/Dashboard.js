05fdb30aa6e29c184ad4b37028918ef859539546
teodora1maximiuc <teodoramax560@gmail.com>
2024-06-07 10:02:30 +0300

    front notification out

--- front/src/Pages/Dashboard.js
+++ front/src/Pages/Dashboard.js
@@ -20,7 +20,7 @@ function Card({ borderColor, bgColor, imageSrc, title, value, description, viewT
 }
 
 export default function Dashboard() {
-  const months = ['June', 'May', 'April', 'Marchz'];
+  const months = ['June', 'May', 'April', 'March'];
   const [dashboardData, setDashboardData] = useState(null);
   const [selectedMonth, setSelectedMonth] = useState(new Date().getMonth() + 3);
   const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
@@ -77,11 +77,6 @@ export default function Dashboard() {
             <div className="header-name">Stockspital</div>
           </div>
           <div className="header-center">Spital Sf. Maria</div>
-          <div className="header-right">
-            <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/7e038ee69d5e007292339b72556c5ec5e57b20ff249d1a7e300aae51ba5b3bf2?apiKey=166a782ca6344aad902f23b81529b6b9&" alt="Notifications" className="header-notification-icon" />
-            <div className="header-notification-text">Notifications</div>
-            <div className="header-notification-count">01</div>
-          </div>
         </div>
       </header>
       <main className="main">
--- front/src/Pages/Inventory.js
+++ front/src/Pages/Inventory.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const itemsPerPage = 8;
-const totalMedicines = 152; 
+const totalMedicines = 24; 
 const totalPages = Math.ceil(totalMedicines / itemsPerPage); 
 
 function SearchBar() {
@@ -191,11 +191,6 @@ export default function Inventory() {
                         <div className="header-name">Stockspital</div>
                     </div>
                     <div className="header-center">Spital Sf. Maria</div>
-                    <div className="header-right">
-                        <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/7e038ee69d5e007292339b72556c5ec5e57b20ff249d1a7e300aae51ba5b3bf2?apiKey=166a782ca6344aad902f23b81529b6b9&" alt="Notifications" className="header-notification-icon" />
-                        <div className="header-notification-text">Notifications</div>
-                        <div className="header-notification-count">01</div>
-                    </div>
                 </div>
             </header>
             <main className="main-cont">
