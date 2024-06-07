e3db29ff2cd9bb6e9983115cad7f59f1a9fe348a
teodora1maximiuc <teodoramax560@gmail.com>
2024-06-07 08:54:48 +0300

    dropdown

--- back/demo/src/main/java/com/example/demo/Drugs/DrugDAO.java
+++ back/demo/src/main/java/com/example/demo/Drugs/DrugDAO.java
@@ -5,6 +5,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class DrugDAO {
