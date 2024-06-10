aa6673f06f9d4890a67b214ba7dc40617084fa6b
teodora1maximiuc <teodoramax560@gmail.com>
2024-06-10 21:19:42 +0300

    fara illness

--- back/demo/src/main/java/com/example/demo/Drugs/DrugDAO.java
+++ back/demo/src/main/java/com/example/demo/Drugs/DrugDAO.java
@@ -80,8 +80,8 @@ public class DrugDAO {
             Drugs drug = new Drugs();
             drug.setName(rs.getString("drug_name"));
             drug.setDosageForm(rs.getString("dosage_form"));
-            drug.setIllness(rs.getString("illness"));
             drug.setStock(rs.getInt("stock"));
+            drug.setStock(rs.getInt("average_stock"));
             System.out.println("the info:" + drug);
             return drug;
         });
