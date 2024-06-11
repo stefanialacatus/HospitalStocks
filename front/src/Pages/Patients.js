9ceff546297eeb615bdf4dec36312f5fabd0c817
stefanialacatus <126088722+stefanialacatus@users.noreply.github.com>
2024-06-11 21:30:35 +0300

    schimbare navbar

--- front/src/Pages/Patients.js
+++ front/src/Pages/Patients.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import './Patients.css';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import Header from '../Components/Header';
 
 const itemsPerPage = 8;
 
@@ -327,15 +328,7 @@ export default function Patients() {
 
     return (
         <>
-            <header className="header">
-                <div className="header-content">
-                    <div className="header-left">
-                        <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/a0a9d789f882e264c6147f22c9098045cccac58ca47b5236bf41bb05030906c9?apiKey=166a782ca6344aad902f23b81529b6b9&" alt="Stockspital" className="header-logo" />
-                        <div className="header-name">Stockspital</div>
-                    </div>
-                    <div className="header-center">Spital Sf. Maria</div>
-                </div>
-            </header>
+            <Header />
             <main className="main-cont">
                 <section className="introducere">
                     <div className="title-section">
